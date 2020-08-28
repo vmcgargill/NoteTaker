0c279ae6ead16307a95758fa0355e6e626d44b16
Vincent McGargill <vincentmcgargill@gmail.com>
2020-08-28 13:59:36 -0600

    Finished front end to look like the rest of my current portfolio projects. Added get notes method to display notes on page.

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
+const fs = require("fs")
 const PORT = process.env.PORT || 8080;
 const express = require("express");
 const app = express();
-const path = require("path")
+const path = require("path");
+const { json } = require("express");
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  console.log(req)
+  res.sendFile(path.join(__dirname, "public" + req.url + ".html"));
 });
 
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
+  res.sendFile(path.join(__dirname, "public" + req.url + ".html"));
+});
+
+app.get("/api/notes", function(req, res) {
+  let getSavedNotes = JSON.parse(fs.readFileSync(__dirname + "/db/db.json"));
+  res.json(getSavedNotes);
 });
 
 app.listen(PORT, function() {
   console.log("Server is listening on http://localhost:" + PORT);
 });
-
